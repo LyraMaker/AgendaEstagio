6f98d9724014272223df5cbb561648db8f83e7a6
LyraMaker <gabriel.lyrat@gmail.com>
2022-06-04 10:54:05 -0300

    Classe Aluno - Fase de criação

---
+++ application/Entity/Aluno.php
@@ -0,0 +1,49 @@
+<?php
+namespace AgendaEstagio\application\Entity;
+/**
+ * 
+ * @Entity
+ *
+ */
+class Aluno
+{
+    private $matricula;
+    private $ano;
+    private $semestre;
+    public function getMatricula():string
+    {
+        return $this->matricula;
+    }
+
+    public function setMatricula($matricula):self
+    {
+        $this->matricula = $matricula;
+        return $this;
+    }
+
+    public function getAno():string
+    {
+        return $this->ano;
+    }
+
+    public function setAno($ano):self
+    {
+        $this->ano = $ano;
+        return $this;
+    }
+
+    public function getSemestre():string
+    {
+        return $this->semestre;
+        
+    }
+
+    public function setSemestre($semestre):self
+    {
+        $this->semestre = $semestre;
+        return $this;
+    }
+
+    
+}
+
